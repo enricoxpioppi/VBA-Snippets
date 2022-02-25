5f70eb12affeb987cdffa9e58dae5304a9f318a0
enricoxpioppi <enrico.pioppi98@gmail.com>
2022-02-25 15:24:23 +0100

    Open selected file

--- SelectFile.vb
+++ SelectFile.vb
@@ -1,1 +1,5 @@
+'Return file name as String
 Application.GetOpenFilename(filefilter, fileindex, title, MultiSelect)
+
+'Open selected file
+Set NewWorkBook = Workbooks.Open(Application.GetOpenFilename())
